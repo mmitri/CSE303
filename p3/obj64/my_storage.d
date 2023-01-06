9f97a426f821e369eba360218adf2744873b6a0c
mmitri <88552429+mmitri@users.noreply.github.com>
2023-01-06 12:10:07 -0500

    Add files via upload

---
+++ p3/obj64/my_storage.d
@@ -0,0 +1,4 @@
+obj64/my_storage.o: server/my_storage.cc \
+ server/../common/contextmanager.h server/../common/err.h \
+ server/../common/protocol.h server/authtableentry.h server/format.h \
+ server/map.h server/map_factories.h server/persist.h server/storage.h
---
+++ p3/obj64/persist.d
@@ -0,0 +1,1 @@
+obj64/persist.o: server/persist.cc server/persist.h
